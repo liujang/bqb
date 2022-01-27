e161480a685a006f1c805732585a63ac4e306423
liujang <41243924+liujang@users.noreply.github.com>
2022-01-27 11:46:30 +0800

    Update huan.sh

--- huan.sh
+++ huan.sh
@@ -6,9 +6,10 @@ BLUE="\033[0;36m"
 FUCHSIA="\033[0;35m"
 echo "export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:$PATH" >> ~/.bashrc
 source ~/.bashrc
-cd frp_0.38.0_linux_amd64 && mv frps /root/ && mv frpc /root/
+cd frp_0.38.0_linux_amd64 && mv frps.ini /root/ && mv frpc.ini /root/
 cd /root/
+rm -rf frp_0.38.0_linux_amd64
 wget https://github.com/fatedier/frp/releases/download/v0.39.0/frp_0.39.0_linux_amd64.tar.gz && tar -xvzf frp_0.39.0_linux_amd64.tar.gz
-cd frp_0.39.0_linux_amd64 && rm -rf frps frpc
+cd frp_0.39.0_linux_amd64 && rm -rf frps.ini frpc.ini
 cd /root/
-mv frps /root/frp_0.39.0_linux_amd64/ && mv frpc /root/frp_0.39.0_linux_amd64/
+mv frps.ini /root/frp_0.39.0_linux_amd64/ && mv frpc.ini /root/frp_0.39.0_linux_amd64/
