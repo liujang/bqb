342f1703c3ead8b0d91ede4d6c7dea3bd494b930
liujang <41243924+liujang@users.noreply.github.com>
2022-02-12 11:16:37 +0800

    Update newtest.sh

--- newtest.sh
+++ newtest.sh
@@ -36,25 +36,25 @@ if [[ -f /etc/redhat-release ]]; then
   fi
   # PM='apt'
   if [ $PM = 'apt' ] ; then
-    apt-get install cron unzip lsof wget curl -y
+    apt-get install -y cron
     service cron start
 elif [ $PM = 'yum' ]; then 
     yum install -y vixie-cron
     yum install -y crontabs
-    yum install unzip lsof wget curl -y
     service cron start
 fi
 echo -e "
  ${GREEN} 1.centos禁用selinux
- ${GREEN} 2.对接ssr
+ ${GREEN} 2.对接ss
  ${GREEN} 3.安装nginx(debian&ubuntu)
  ${GREEN} 4.安装nginx(centos)
  ${GREEN} 5.申请ssl证书
- ${GREEN} 6.tcp隧道(tls)
+ ${GREEN} 6.udp伪装加速隧道
  ${GREEN} 7.安装内核
  ${GREEN} 8.删除防火墙
  ${GREEN} 9.增加swap
- ${GREEN} 10.关闭udp
+ ${GREEN} 10.安装udp隧道工具
+ ${GREEN} 11.tcp隧道(tls)
  "
  read -p "输入选项:" aNum
  if [ "$aNum" = "1" ];then
@@ -63,133 +63,93 @@ echo "3s后重启系统"
 sleep 3
 reboot
 elif [ "$aNum" = "2" ] ;then
-if [[ "$EUID" -ne 0 ]]; then
-    echo "false"
-  else
-    echo "true"
-  fi
-if [[ -f /etc/redhat-release ]]; then
-		release="centos"
-	elif cat /etc/issue | grep -q -E -i "debian"; then
-		release="debian"
-	elif cat /etc/issue | grep -q -E -i "ubuntu"; then
-		release="ubuntu"
-	elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
-		release="centos"
-	elif cat /proc/version | grep -q -E -i "debian"; then
-		release="debian"
-	elif cat /proc/version | grep -q -E -i "ubuntu"; then
-		release="ubuntu"
-	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
-		release="centos"
-    fi
-    
-     if [[ $release = "ubuntu" || $release = "debian" ]]; then
-    PM='apt'
-  elif [[ $release = "centos" ]]; then
-    PM='yum'
-  else
-    exit 1
-  fi
-  # PM='apt'
-  if [ $PM = 'apt' ] ; then
-    apt-get update -y
-    apt-get install vim curl git wget zip unzip python3 python3-pip git -y
-    apt-get install -y cron
-    service cron start
-    apt install net-tools -y
-    apt install debian-keyring debian-archive-keyring apt-transport-https -y
-    curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | apt-key add -
-    curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee -a /etc/apt/sources.list.d/caddy-stable.list
-    apt-get update -y
-    apt install caddy -y
-elif [ $PM = 'yum' ]; then
-    yum update -y
-    systemctl stop initial-setup-text
-    yum install net-tools -y
-    yum install vim curl git wget zip unzip python3 python3-pip git -y
-    yum install -y vixie-cron
-    yum install -y crontabs
-    service cron start
-    yum install yum-plugin-copr -y
-    yum copr enable @caddy/caddy -y
-    yum install caddy -y
-    sed -i '1s/python/'python2'/' /bin/yum
-    sed -i '1s/python22/'python2'/' /bin/yum
-    sed -i '1s/python/'python2'/' /usr/libexec/urlgrabber-ext-down
-    sed -i '1s/python22/'python2'/' /usr/libexec/urlgrabber-ext-down
-fi
-kill -9 $(netstat -nlp | grep :2019 | awk '{print $7}' | awk -F"/" '{ print $1 }')
-kill -9 $(netstat -nlp | grep :80 | awk '{print $7}' | awk -F"/" '{ print $1 }')
-kill -9 $(netstat -nlp | grep :443 | awk '{print $7}' | awk -F"/" '{ print $1 }')
+bash <(curl -Ls https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh)
 cd
-pip3 install --upgrade pip
-echo -e
-git clone https://github.com/522707900/test.git
-echo -e
-cd test
-pip3 install -r requirements.txt
-sleep 5
-cp apiconfig.py userapiconfig.py && cp config.json user-config.json
-echo -e
-read -p "请输入后端多少小时测速一次(默认720小时，一个月):" speedtestnum
- [ -z "${speedtestnum}" ] && speedtestnum=720
-    echo
-    echo "---------------------------"
-    echo "speedtestnum = ${speedtestnum}"
-    echo "---------------------------"
-    sed -i '5s/6/'${speedtestnum}'/' userapiconfig.py
-    echo
-echo -e "
- ${GREEN} 1.web
- ${GREEN} 2.db
- "
- read -p "输入你要的对接方式:" aNum
-if [ "$aNum" = "1" ];then
-read -p "请输入网站域名(末尾不要有/,列如www.baidu.com):" webapi1
-sleep 1
-sed -i '16s/123456/'${webapi1}'/' userapiconfig.py
-read -p "请输入网站mukey:" key
- echo "网站mukey为：${key}"
- sleep 1
- sed -i '17s/123/'${key}'/' userapiconfig.py
- read -p "请输入节点序号:" node
- echo "节点序号为：${node}"
- sleep 1
- sed -i '2s/0/'${node}'/' userapiconfig.py
- elif [ "$aNum" = "2" ] ;then
- sed -i '14s/modwebapi/glzjinmod/' userapiconfig.py
- read -p "请输入数据库地址:" ip
- echo "数据库地址为：${ip}"
- sleep 1
- sed -i '23s/127.0.0.1/'${ip}'/' userapiconfig.py
- read -p "请输入数据库用户名:" user
- echo "数据库用户名为：${user}"
- sleep 1
- sed -i '25s/ss/'${user}'/' userapiconfig.py
- read -p "请输入数据库名:" db
- echo "数据库名为：${db}"
- sleep 1
- sed -i '27s/shadowsocks/'${db}'/' userapiconfig.py
- read -p "请输入数据库密码:" passwd
- echo "数据库密码为：${passwd}"
- sleep 1
- sed -i '26s/ss/'${passwd}'/' userapiconfig.py
- read -p "请输入节点序号:" node
- echo "节点序号为：${node}"
- sleep 1
- sed -i '2s/0/'${node}'/' userapiconfig.py
-  else
-            echo "你他妈是猪吗，就两个数字给你选，你都选错，滚！！！"
-            fi
+rm -rf /etc/XrayR/config.yml
+read -p "输入对接域名(例如www.baidu.com):" ym
+read -p "输入节点id:" nodeid
+read -p "输入mukey:" mukey
+echo "
+Log:
+  Level: none # Log level: none, error, warning, info, debug 
+  AccessPath: # /etc/XrayR/access.Log
+  ErrorPath: # /etc/XrayR/error.log
+DnsConfigPath: # /etc/XrayR/dns.json # Path to dns config, check https://xtls.github.io/config/base/dns/ for help
+RouteConfigPath: # /etc/XrayR/route.json # Path to route config, check https://xtls.github.io/config/base/route/ for help
+OutboundConfigPath: # /etc/XrayR/custom_outbound.json # Path to custom outbound config, check https://xtls.github.io/config/base/outbound/ for help
+ConnetionConfig:
+  Handshake: 4 # Handshake time limit, Second
+  ConnIdle: 30 # Connection idle time limit, Second
+  UplinkOnly: 2 # Time limit when the connection downstream is closed, Second
+  DownlinkOnly: 4 # Time limit when the connection is closed after the uplink is closed, Second
+  BufferSize: 64 # The internal cache size of each connection, kB 
+Nodes:
+  -
+    PanelType: "SSpanel" # Panel type: SSpanel, V2board, PMpanel, , Proxypanel
+    ApiConfig:
+      ApiHost: "https://${ym}"
+      ApiKey: "${mukey}"
+      NodeID: ${nodeid}
+      NodeType: Shadowsocks # Node type: V2ray, Shadowsocks, Trojan, Shadowsocks-Plugin
+      Timeout: 30 # Timeout for the api request
+      EnableVless: false # Enable Vless for V2ray Type
+      EnableXTLS: false # Enable XTLS for V2ray and Trojan
+      SpeedLimit: 0 # Mbps, Local settings will replace remote settings, 0 means disable
+      DeviceLimit: 0 # Local settings will replace remote settings, 0 means disable
+      RuleListPath: # ./rulelist Path to local rulelist file
+    ControllerConfig:
+      ListenIP: 0.0.0.0 # IP address you want to listen
+      SendIP: 0.0.0.0 # IP address you want to send pacakage
+      UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
+      EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
+      DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
+      EnableProxyProtocol: false # Only works for WebSocket and TCP
+      EnableFallback: false # Only support for Trojan and Vless
+      FallBackConfigs:  # Support multiple fallbacks
+        -
+          SNI: # TLS SNI(Server Name Indication), Empty for any
+          Path: # HTTP PATH, Empty for any
+          Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/fallback/ for details.
+          ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
+      CertConfig:
+        CertMode: none # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
+        CertDomain: "node1.test.com" # Domain to cert
+        CertFile: ./cert/node1.test.com.cert # Provided if the CertMode is file
+        KeyFile: ./cert/node1.test.com.key
+        Provider: alidns # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
+        Email: test@me.com
+        DNSEnv: # DNS ENV option used by DNS provider
+          ALICLOUD_ACCESS_KEY: aaa
+          ALICLOUD_SECRET_KEY: bbb
+  # -
+  #   PanelType: "V2board" # Panel type: SSpanel, V2board
+  #   ApiConfig:
+  #     ApiHost: "http://127.0.0.1:668"
+  #     ApiKey: "123"
+  #     NodeID: 4
+  #     NodeType: Shadowsocks # Node type: V2ray, Shadowsocks, Trojan
+  #     Timeout: 30 # Timeout for the api request
+  #     EnableVless: false # Enable Vless for V2ray Type
+  #     EnableXTLS: false # Enable XTLS for V2ray and Trojan
+  #     SpeedLimit: 0 # Mbps, Local settings will replace remote settings
+  #     DeviceLimit: 0 # Local settings will replace remote settings
+  #   ControllerConfig:
+  #     ListenIP: 0.0.0.0 # IP address you want to listen
+  #     UpdatePeriodic: 10 # Time to update the nodeinfo, how many sec.
+  #     EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
+  #     CertConfig:
+  #       CertMode: dns # Option about how to get certificate: none, file, http, dns
+  #       CertDomain: "node1.test.com" # Domain to cert
+  #       CertFile: ./cert/node1.test.com.cert # Provided if the CertMode is file
+  #       KeyFile: ./cert/node1.test.com.pem
+  #       Provider: alidns # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
+  #       Email: test@me.com
+  #       DNSEnv: # DNS ENV option used by DNS provider
+  #         ALICLOUD_ACCESS_KEY: aaa
+  #         ALICLOUD_SECRET_KEY: bbb
+" > /etc/XrayR/config.yml
 cd
-rm -rf /usr/bin/python
-ln -s /usr/bin/python3  /usr/bin/python
-rm -rf test/user-config.json
-cd test && wget -N --no-check-certificate "https://github.91chi.fun//https://raw.githubusercontent.com/liujang/foc2/main/user-config.json"
-cd
-cd test && chmod +x run.sh && ./run.sh
-echo "已经对接完成！！!。"
+XrayR restart
 cd
 elif [ "$aNum" = "3" ] ;then
 apt-get update -y && apt install nginx -y
@@ -232,46 +192,28 @@ echo -e "
  ${GREEN} 1.落地机
  ${GREEN} 2.中转机
  "
-read -p "输入选项:" cNum
-if [ "$cNum" = "1" ] ;then
-read -p "输入域名:" nodeym1
-read -p "输入被转发的端口:" nodeport
-read -p "输入监听端口(外网)1:" ngport1
-sed -i '$d' /etc/nginx/nginx.conf
+read -p "输入选项:" bNum
+if [ "$bNum" = "1" ];then
+read -p "输入被加速的udp端口1:" udpport1
+read -p "输入监听的udp端口2(外网):" udpport2
+nohup pingtunnel -type server >> /dev/null 2>&1 &
+nohup speederv2_amd64 -s -l0.0.0.0:${udpport2} -r127.0.0.1:${udpport1}  -f20:10 -k "passwd" --mode 0 >> /dev/null 2>&1 &
 echo "
-server {
-        listen ${ngport1} ssl;
-        ssl_protocols TLSv1.2;
-        ssl_certificate /home/ssl/${nodeym1}/1.pem; # 证书地址
-	      ssl_certificate_key /home/ssl/${nodeym1}/1.key; # 秘钥地址
-        ssl_session_tickets off;
-        ssl_prefer_server_ciphers on;  # prefer a list of ciphers to prevent old and slow ciphers
-        ssl_ciphers 'EECDH+AESGCM';
-        proxy_pass 127.0.0.1:${nodeport};
-    }
-    } " >> /etc/nginx/nginx.conf
-    echo "落地nginx tcp端口为:${ngport1}"
-    elif [ "$cNum" = "2" ] ;then
-    read -p "输入落地nginx ip:" ngip1
-    read -p "输入落地nginx 域名:" nodeym2
-    read -p "输入落地nginx tcp端口2:" ngport2
-    read -p "输入中转监听 tcp端口:" zzport
-    sed -i '$d' /etc/nginx/nginx.conf
-    echo "
-    server {
-        listen ${zzport};
-        proxy_ssl on;
-        proxy_ssl_protocols TLSv1.2;
-        proxy_ssl_server_name on;
-        proxy_ssl_name ${nodeym2};
-        proxy_pass ${ngip1}:${ngport2};
-    }
-    } " >> /etc/nginx/nginx.conf
-    echo "中转监听的tcp端口为:${zzport}"
+nohup speederv2_amd64 -s -l0.0.0.0:${udpport2} -r127.0.0.1:${udpport1}  -f20:10 -k "passwd" --mode 0 >> /dev/null 2>&1 &
+" >> ./ziqi.sh
+echo "落地机udp伪装加速的外网端口为:${udpport2}"
+elif [ "$bNum" = "2" ] ;then
+read -p "输入落地机的外网ip:" ip1
+read -p "输入落地机udp伪装加速的外网端口:" tcpport2
+read -p "输入中转机监听的udp端口4（外网）:" udpport4
+nohup pingtunnel -type client -l 127.0.0.1:${tcpport2} -s ${ip1} -t ${ip1}:${tcpport2} >> /dev/null 2>&1 &
+nohup speederv2_amd64 -c -l0.0.0.0:${udpport4} -r127.0.0.1:${tcpport2} -f20:10 -k "passwd" --mode 0 >> /dev/null 2>&1 &
+echo "
+nohup pingtunnel -type client -l 127.0.0.1:${tcpport2} -s ${ip1} -t ${ip1}:${tcpport2} >> /dev/null 2>&1 &
+nohup speederv2_amd64 -c -l0.0.0.0:${udpport4} -r127.0.0.1:${tcpport2} -f20:10 -k "passwd" --mode 0 >> /dev/null 2>&1 &
+" >> ./ziqi.sh
+echo "最终的外网udp端口为:${udpport4}"
 fi
-sleep 1
-systemctl restart nginx
-cd
 elif [ "$aNum" = "7" ] ;then
 wget -N --no-check-certificate "https://raw.githubusercontent.com/ylx2016/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh && ./tcp.sh
 elif [ "$aNum" = "8" ] ;then
@@ -336,8 +278,61 @@ echo " ###########设置开机自启动############"
 echo '/mnt/swap swap swap defaults 0 0' >> /etc/fstab
 echo "All done！Thanks for using this shell script"
 elif [ "$aNum" = "10" ] ;then
-read -p "输入要屏蔽的udp端口:" pbudpport
-iptables -A INPUT -p udp --dport ${pbudpport} -j DROP
-iptables -A OUTPUT -p udp --dport ${pbudpport} -j DROP
-service iptables save
+wget -N --no-check-certificate "https://github.91chi.fun//https://raw.githubusercontent.com/liujang/bqb/main/ziqi.sh" && chmod +x ziqi.sh
+mkdir udp && cd udp
+wget https://github.91chi.fun//https://github.com//esrrhs/pingtunnel/releases/download/2.6/pingtunnel_linux_amd64.zip
+wget https://github.91chi.fun//https://github.com/wangyu-/UDPspeeder/releases/download/20210116.0/speederv2_binaries.tar.gz
+unzip pingtunnel_linux_amd64.zip
+tar -xzvf speederv2_binaries.tar.gz
+mv pingtunnel /usr/bin/ && chmod +x /usr/bin/pingtunnel
+mv speederv2_amd64 /usr/bin/ && chmod +x /usr/bin/speederv2_amd64
+export PATH="$PATH:/usr/bin"
+cd
+crontab -l > conf
+echo "@reboot ./ziqi.sh" >> conf
+crontab conf
+rm -f conf
+echo "已设置开机自动运行udp隧道"
+elif [ "$aNum" = "11" ] ;then
+echo -e "
+ ${GREEN} 1.落地机
+ ${GREEN} 2.中转机
+ "
+read -p "输入选项:" cNum
+if [ "$cNum" = "1" ] ;then
+read -p "输入域名:" nodeym1
+read -p "输入被转发的端口:" nodeport
+read -p "输入监听端口(外网)1:" ngport1
+sed -i '$d' /etc/nginx/nginx.conf
+echo "
+server {
+        listen ${ngport1} ssl;
+        ssl_protocols TLSv1.3;
+        ssl_certificate /home/ssl/${nodeym1}/1.pem; # 证书地址
+	ssl_certificate_key /home/ssl/${nodeym1}/1.key; # 秘钥地址
+        proxy_pass 127.0.0.1:${nodeport};
+    }
+    } " >> /etc/nginx/nginx.conf
+    echo "落地nginx tcp端口为:${ngport1}"
+    elif [ "$cNum" = "2" ] ;then
+    read -p "输入落地nginx ip:" ngip1
+    read -p "输入落地nginx 域名:" nodeym2
+    read -p "输入落地nginx tcp端口2:" ngport2
+    read -p "输入中转监听 tcp端口:" zzport
+    sed -i '$d' /etc/nginx/nginx.conf
+    echo "
+    server {
+        listen ${zzport};
+        proxy_ssl on;
+        proxy_ssl_protocols TLSv1.3;
+        proxy_ssl_server_name on;
+        proxy_ssl_name ${nodeym2};
+        proxy_pass ${ngip1}:${ngport2};
+    }
+    } " >> /etc/nginx/nginx.conf
+    echo "中转监听的tcp端口为:${zzport}"
+fi
+sleep 1
+systemctl restart nginx
+cd
 fi
