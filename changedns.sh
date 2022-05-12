6f658cd3ceb950f77850d74127f3e902a2b6f399
liujang <41243924+liujang@users.noreply.github.com>
2022-05-12 16:16:28 +0800

    Update changedns.sh

--- changedns.sh
+++ changedns.sh
@@ -6,7 +6,9 @@ BLUE="\033[0;36m"
 FUCHSIA="\033[0;35m"
 echo "export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:$PATH" >> ~/.bashrc
 source ~/.bashrc
+wget -P /etc "https://h5ai.ljfxz.net/bqbdns/area/resolv.conf.1"
 rm -rf /etc/resolv.conf
-wget -P /etc "https://h5ai.ljfxz.net/bqbdns/area/resolv.conf"
+cp /etc/resolv.conf.1 /etc/resolv.conf
+rm -rf /etc/resolv.conf.1
 systemctl restart NetworkManager.service
 systemctl restart systemd-resolved.service
