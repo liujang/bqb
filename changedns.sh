f849dc9cf18ebd3a6a0dad2497b8f0104ba8d9d4
liujang <41243924+liujang@users.noreply.github.com>
2022-05-12 16:38:59 +0800

    Update changedns.sh

--- changedns.sh
+++ changedns.sh
@@ -11,5 +11,37 @@ sleep 3
 rm -rf /etc/resolv.conf
 cp /etc/resolv.conf.1 /etc/resolv.conf
 rm -rf /etc/resolv.conf.1
+if [[ "$EUID" -ne 0 ]]; then
+    echo "false"
+  else
+    echo "true"
+  fi
+if [[ -f /etc/redhat-release ]]; then
+		release="centos"
+	elif cat /etc/issue | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+	elif cat /proc/version | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /proc/version | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+    fi
+    
+     if [[ $release = "ubuntu" || $release = "debian" ]]; then
+    PM='apt'
+  elif [[ $release = "centos" ]]; then
+    PM='yum'
+  else
+    exit 1
+  fi
+  # PM='apt'
+  if [ $PM = 'apt' ] ; then
+  systemctl restart systemd-resolved.service
+elif [ $PM = 'yum' ]; then 
 systemctl restart NetworkManager.service
-systemctl restart systemd-resolved.service
+fi
