2c0473de7e665d73974e6d0546f44f42b1116f06
liujang <41243924+liujang@users.noreply.github.com>
2022-05-12 14:36:30 +0800

    Create bqbdns.sh

---
+++ bqbdns.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+RED_COLOR="\033[0;31m"
+NO_COLOR="\033[0m"
+GREEN="\033[32m\033[01m"
+BLUE="\033[0;36m"
+FUCHSIA="\033[0;35m"
+echo "export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:$PATH" >> ~/.bashrc
+source ~/.bashrc
+if [[ "$EUID" -ne 0 ]]; then
+    echo "false"
+  else
+    echo "true"
+  fi
+if [[ -f /etc/redhat-release ]]; then
+		release="centos"
+	elif cat /etc/issue | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+	elif cat /proc/version | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /proc/version | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+    fi
+    
+     if [[ $release = "ubuntu" || $release = "debian" ]]; then
+    PM='apt'
+  elif [[ $release = "centos" ]]; then
+    PM='yum'
+  else
+    exit 1
+  fi
+  # PM='apt'
+  if [ $PM = 'apt' ] ; then
+    apt-get install -y cron
+    service cron start
+elif [ $PM = 'yum' ]; then 
+    yum install -y vixie-cron
+    yum install -y crontabs
+    service cron start
+fi
+echo -e "
+ ${GREEN} 1.hk
+ ${GREEN} 2.jp
+ ${GREEN} 3.sgp
+ ${GREEN} 4.us
+ "
+  read -p "输入选项:" aNum
+ if [ "$aNum" = "1" ];then
+ elif [ "$aNum" = "2" ];then
+ elif [ "$aNum" = "3" ];then
+ elif [ "$aNum" = "4" ];then
+ fi
