69acaada614100312eb31c4a35436b836fbd46a2
liujang <41243924+liujang@users.noreply.github.com>
2022-06-11 00:08:15 +0800

    Update changedns.sh

--- changedns.sh
+++ changedns.sh
@@ -8,41 +8,6 @@ echo "export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~
 source ~/.bashrc
 wget -N --no-check-certificate -P /etc "https://h5ai.ljfxz.net/bqbdns/area/resolv.conf.1"
 sleep 3
-rm -rf /etc/resolv.conf
-cp /etc/resolv.conf.1 /etc/resolv.conf
+bqbdns=`head -n +1 /etc/resolv.conf.1`
+./dns-change.sh &bqbdns
 rm -rf /etc/resolv.conf.1
-if [[ "$EUID" -ne 0 ]]; then
-    echo "false"
-  else
-    echo "true"
-  fi
-if [[ -f /etc/redhat-release ]]; then
-		release="centos"
-	elif cat /etc/issue | grep -q -E -i "debian"; then
-		release="debian"
-	elif cat /etc/issue | grep -q -E -i "ubuntu"; then
-		release="ubuntu"
-	elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
-		release="centos"
-	elif cat /proc/version | grep -q -E -i "debian"; then
-		release="debian"
-	elif cat /proc/version | grep -q -E -i "ubuntu"; then
-		release="ubuntu"
-	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
-		release="centos"
-    fi
-    
-     if [[ $release = "ubuntu" || $release = "debian" ]]; then
-    PM='apt'
-  elif [[ $release = "centos" ]]; then
-    PM='yum'
-  else
-    exit 1
-  fi
-  # PM='apt'
-  if [ $PM = 'apt' ] ; then
-  systemctl daemon-reload
-systemctl restart systemd-resolved.service
-elif [ $PM = 'yum' ]; then
-systemctl restart NetworkManager.service
-fi
