3d6558ac51fc7db97730ae639789249793536877
liujang <41243924+liujang@users.noreply.github.com>
2022-10-22 18:25:10 +0800

    Update addip.sh

--- addip.sh
+++ addip.sh
@@ -4,7 +4,7 @@ gateway=`route -n | grep UG | awk '{print $2}'`
 genmask=`ifconfig eth0 | grep broadcast | awk '{print $4}'`
 read -p "添加的ip(不可重复):" ip
 read -p "输入网卡序号(不可重复):" Num
-echp "
+echo "
 auto eth0:$Num
 iface eth0:$Num inet static
 address $ip
