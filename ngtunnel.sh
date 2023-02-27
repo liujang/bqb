ed49e6c4f2cf0dddb0f165977465c733e70dee63
liujang <41243924+liujang@users.noreply.github.com>
2023-02-27 10:24:13 +0800

    Update ngtunnel.sh

--- ngtunnel.sh
+++ ngtunnel.sh
@@ -2,3 +2,17 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 clear
+
+RED_COLOR="\033[0;31m"
+NO_COLOR="\033[0m"
+GREEN="\033[32m\033[01m"
+BLUE="\033[0;36m"
+FUCHSIA="\033[0;35m"
+
+check_nginx(){
+if test -a /usr/sbin -a /etc/nginx/nginx.conf;then
+        echo -e "--------nginx已安装--------"
+    else
+        echo -e "--------nginx未安装---------"
+    fi
+}
