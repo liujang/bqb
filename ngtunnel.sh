d5db5f9562c2eb7a6730130291d8359c6ad5c587
liujang <41243924+liujang@users.noreply.github.com>
2023-02-27 13:36:19 +0800

    Update ngtunnel.sh

--- ngtunnel.sh
+++ ngtunnel.sh
@@ -155,10 +155,10 @@ server {
         ssl_session_timeout 2h;
         ssl_session_tickets off;
         tcp_nodelay on;
-        proxy_pass '$node';
+        proxy_pass $node;
         resolver 1.1.1.1 8.8.8.8 valid=30s;
         resolver_timeout 3s;
-        set '$node' "${remote_ip}:${remote_port}";
+        set $node "${remote_ip}:${remote_port}";
 	proxy_protocol off;
         access_log off;
 }
@@ -178,10 +178,10 @@ server {
         ssl_session_tickets off;
         tcp_nodelay on;
         proxy_ssl_name ${remote_ip};
-        proxy_pass '$node';
+        proxy_pass $node;
         resolver 223.5.5.5 119.29.29.29 valid=30s;
         resolver_timeout 3s;
-        set '$node' "${remote_ip}:${remote_port}";
+        set $node "${remote_ip}:${remote_port}";
 	proxy_protocol off;
         access_log off;
 }
