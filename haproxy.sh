1ca7e86417d8d1dcd5ad0a61cdea43c7dc75299b
liujang <41243924+liujang@users.noreply.github.com>
2023-04-30 21:06:23 +0800

    Create haproxy.sh

---
+++ haproxy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+clear
+
+RED_COLOR="\033[0;31m"
+NO_COLOR="\033[0m"
+GREEN="\033[32m\033[01m"
+BLUE="\033[0;36m"
+FUCHSIA="\033[0;35m"
+
+lua_da=https://www.lua.org/ftp/lua-5.4.5.tar.gz
+haproxy_da=https://github.com/haproxy/haproxy/archive/refs/tags/v2.8-dev9.tar.gz
