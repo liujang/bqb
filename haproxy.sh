967be9d603f41ad28069f238543d8741cd7b0c0c
liujang <41243924+liujang@users.noreply.github.com>
2023-04-30 21:39:00 +0800

    Update haproxy.sh

--- haproxy.sh
+++ haproxy.sh
@@ -10,6 +10,20 @@ BLUE="\033[0;36m"
 FUCHSIA="\033[0;35m"
 
 lua_v=lua-5.4.5
-haproxy_v=v2.8-dev9
-lua_da=https://www.lua.org/ftp/lua-5.4.5.tar.gz
-haproxy_da=https://github.com/haproxy/haproxy/archive/refs/tags/v2.8-dev9.tar.gz
+haproxy_v=2.8-dev9
+
+install_haproxy(){
+apt update -y && apt install vim curl lsof wget -y
+apt install build-essential libpcre3 libpcre3-dev zlib1g-dev openssl libssl-dev -y
+wget https://www.lua.org/ftp/${lua_v}.tar.gz
+wget https://github.com/haproxy/haproxy/archive/refs/tags/v${haproxy_v}.tar.gz
+tar -xvzf ${lua_v}.tar.gz && tar -xvzf v${haproxy_v}.tar.gz
+cd /root/${lua_v}
+make linux
+make install INSTALL_TOP=/usr/local/lua
+cd /root/haproxy-${haproxy_v}
+make TARGET=linux-glibc USE_PCRE=1 USE_OPENSSL=1 USE_ZLIB=1 USE_SYSTEMD=1 USE_CPU_AFFINITY=1 USE_LUA=1 LUA_INC=/usr/local/lua/include LUA_LIB=/usr/local/lua/lib
+make install PREFIX=/usr/local/haproxy
+
+
+}
