a5391c0ccbf93b3ca98905504bd6bdef5580df09
liujang <41243924+liujang@users.noreply.github.com>
2023-05-01 16:28:09 +0800

    Update haproxy.sh

--- haproxy.sh
+++ haproxy.sh
@@ -40,4 +40,19 @@ systemctl daemon-reload
 cd /root/ && rm -rf ${lua_v}.tar.gz v${haproxy_v}.tar.gz ${lua_v} haproxy-${haproxy_v}
 }
 
+haproxy_conf(){
+haproxy_rows=`wc -l /usr/local/haproxy/haproxy.txt | awk '{print $1}'`
+for((i=1;i<=$haproxy_rows;i++));  
+do
+listen_ip=`sed -n "$i, 1p" /usr/local/haproxy/haproxy.txt | awk '{print $1}'`
+listen_port=`sed -n "$i, 1p" /usr/local/haproxy/haproxy.txt | awk '{print $2}'`
+remote_ip=`sed -n "$i, 1p" /usr/local/haproxy/haproxy.txt | awk '{print $3}'`
+remote_port=`sed -n "$i, 1p" /usr/local/haproxy/haproxy.txt | awk '{print $4}'`
+echo -e "listen $listen_port
+   bind $listen_ip:$listen_port ssl crt /etc/nginx/ssl/server.pem verify required ca-file /etc/nginx/ssl/ca1.crt alpn h2
+   server s$listen_port $remote_ip:$remote_port
+" >> /usr/local/haproxy/haproxy.cfg
+done  
+}
+
 install_haproxy
