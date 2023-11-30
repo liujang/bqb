d15d28c9a9b9e683a868f725454afd72e5eb8a7a
liujang <41243924+liujang@users.noreply.github.com>
2023-11-30 22:34:52 +0800

    Update ljfxz1.sh

--- ljfxz1.sh
+++ ljfxz1.sh
@@ -3,10 +3,6 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 clear
 
-curl -fsSL https://get.docker.com | bash -s docker
-curl -L "https://github.com/docker/compose/releases/download/1.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
 if [[ "$EUID" -ne 0 ]]; then
     echo "false"
   else
