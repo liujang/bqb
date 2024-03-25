d1d16c7e4bb9f3dbfbfe14f4d043bde35de9af27
liujang <41243924+liujang@users.noreply.github.com>
2024-03-25 11:30:15 +0800

    Update goit.sh

--- goit.sh
+++ goit.sh
@@ -5,15 +5,16 @@ export PATH
 gre_open(){
 modprobe ip_gre
 }
+
 iptablse_install(){
 if [[ ${iptables_exist} != "" ]]; then
-		echo -e "已经安装iptables"
-	else
-		apt-get update && apt-get install -y iptables
-    iptables -P INPUT ACCEPT
-    iptables -P OUTPUT ACCEPT
-    iptables -P FORWARD ACCEPT
-    iptables-save > /etc/iptables.up.rules
-    iptables-restore < /etc/iptables.up.rules
+echo -e "已经安装iptables"
+else
+apt-get update && apt-get install -y iptables
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables-save > /etc/iptables.up.rules
+iptables-restore < /etc/iptables.up.rules
 }
-}
+
