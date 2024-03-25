be86f5524500294f25bca468396d5070204f3c1c
liujang <41243924+liujang@users.noreply.github.com>
2024-03-26 07:52:56 +0800

    Update goit.sh

--- goit.sh
+++ goit.sh
@@ -22,16 +22,12 @@ sysctl -p
 }
 
 iptables_install(){
-if [[ ${iptables_exist} != "" ]]; then
-echo -e "已经安装iptables"
-else
 apt-get update && apt-get install -y iptables
 iptables -P INPUT ACCEPT
 iptables -P OUTPUT ACCEPT
 iptables -P FORWARD ACCEPT
 iptables-save > /etc/iptables.up.rules
 iptables-restore < /etc/iptables.up.rules
-fi
 }
 
 gre_over_ipsec_set1(){
